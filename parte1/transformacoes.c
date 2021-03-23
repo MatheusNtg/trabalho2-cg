9fdd242282c89a483e20fb4b409cb8582d2f549e
Matheus Rodrigues <matheussr61@gmail.com>
2021-03-23 12:56:05 -0300

    Implementação das coordenas homogêneas

--- parte1/transformacoes.c
+++ parte1/transformacoes.c
@@ -1,51 +1,96 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
-const char* NOME_PROGRAMA = "Transformacoes";
+const char *NOME_PROGRAMA = "Transformacoes";
 void display(void);
 
-typedef float Triangulo[3][2];
-//Triangulo transforma(Triangulo exemplo, outros parametros);
+/* 3 linhas e 3 colunas para lidar com coordenas homogêneas */
+typedef struct triangulo
+{
+    float pontos[3][3];
+} Triangulo;
 
-int main(int argc, char** argv){
+Triangulo transforma(Triangulo exemplo, float transformacao[3][3]);
 
-    glutInit(&argc, argv); // inicializa
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); // cores e buffers
-    glutInitWindowSize (256, 256); // tamanho janela
-    glutInitWindowPosition (100, 100); //posicao janela
-    glutCreateWindow(NOME_PROGRAMA); // cria janela
-    glClearColor(1.0, 1.0, 1.0, 0.0); //cor janela
-    glutDisplayFunc(display); //desenha
+int main(int argc, char **argv)
+{
+
+    glutInit(&argc, argv);                       // inicializa
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // cores e buffers
+    glutInitWindowSize(256, 256);                // tamanho janela
+    glutInitWindowPosition(100, 100);            //posicao janela
+    glutCreateWindow(NOME_PROGRAMA);             // cria janela
+    glClearColor(1.0, 1.0, 1.0, 0.0);            //cor janela
+    glutDisplayFunc(display);                    //desenha
     glutMainLoop();
 
     return 0;
 }
 
-//Triangulo transforma(Triangulo exemplo, outros parametros){
-//   for (int lado = 0; lado < 3; lado++){
-        //exemplo[lado] *= matriz_de_transformacao;
-//   }
-
-//    retorna exemplo;
-//}
-
-void display(void){
-    glClear(GL_COLOR_BUFFER_BIT); //limpa janela
-    glColor3f(0.0, 0.0, 0.0); //cor linha
-
-    Triangulo exemplo = {
-        {0.25, 0.25},
-        {0.75, 0.25},
-        {0.75, 0.75},
+Triangulo transforma(Triangulo exemplo, float transformacao[3][3])
+{
+    // Zera os pontos para não pegar lixo de memória
+    Triangulo result = {
+        .pontos = {0}
     };
 
-    //Desenha Triangulo
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            for (int k = 0; k < 3; ++k)
+            {
+                result.pontos[i][j] += exemplo.pontos[i][k] * transformacao[k][j];
+            }
+        }
+    }
+
+    return result;
+}
+
+void print_matrix(float matrix[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int k = 0; k < 3; k++) {
+            printf("%f ", matrix[i][k]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT); //limpa janela
+    glColor3f(0.0, 0.0, 0.0);     //cor linha
+
+    Triangulo exemplo = {
+        .pontos = {
+            {0.25, 0.25, 1},
+            {0.75, 0.25, 1},
+            {0.75, 0.75, 1},
+        }};
+
+    float matriz_transformacao[3][3] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {-.7, 0, 1}
+    };
+
+    // Desenha Triangulo
     glBegin(GL_TRIANGLES);
-    for (int lado = 0; lado < 3; lado++){
-        glVertex2f(exemplo[lado][0], exemplo[lado][1]);
+    for (int lado = 0; lado < 3; lado++)
+    {
+        glVertex2f(exemplo.pontos[lado][0], exemplo.pontos[lado][1]);
     }
     glEnd();
 
-    //Triangulo = transforma(exemplo)
+    Triangulo t = transforma(exemplo, matriz_transformacao);
+    print_matrix(t.pontos);
+    glBegin(GL_TRIANGLES);
+    for (int lado = 0; lado < 3; lado++)
+    {
+        glVertex2f(t.pontos[lado][0], t.pontos[lado][1]);
+    }
+    glEnd();
     glFlush();
 }
